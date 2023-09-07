ecca35a0a315335d721478904570cc5ce76a1a4c
Josh Hawk <hawkjosh@icloud.com>
2023-09-07 18:39:27 -0400

    updated schedule component to consume external api instead of local json

--- app/team/schedule/ScheduleList.jsx
+++ app/team/schedule/ScheduleList.jsx
@@ -2,23 +2,102 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useDateFormat } from '@/app/utils/useDateFormat'
 
-const homeLogo =
-	'https://www.mlbstatic.com/team-logos/team-cap-on-light/144.svg'
+// const homeLogo =
+// 	'https://www.mlbstatic.com/team-logos/team-cap-on-light/144.svg'
+
+// async function getSchedule() {
+// 	const data = await fetch('http://localhost:4000/schedule', {
+// 		next: {
+// 			revalidate: 0,
+// 		},
+// 	})
+
+// 	if (!data.ok) {
+// 		throw new Error('Failed to fetch data')
+// 	}
+
+// 	return data.json()
+// }
 
 async function getSchedule() {
-	const data = await fetch('http://localhost:4000/schedule', {
+	const url = 'https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=2&season=2023';
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'd402ff8d8amshb632d3dff23fa99p160c6bjsn9fc2d552e490',
+			'X-RapidAPI-Host': 'tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com'
+		}
+	};
+
+	const response = await fetch(url, options, {
 		next: {
 			revalidate: 0,
 		},
 	})
 
-	if (!data.ok) {
+	if (!response.ok) {
 		throw new Error('Failed to fetch data')
 	}
 
-	return data.json()
+	const data = await response.json()
+
+	return data.body.schedule
 }
 
+
+// export default async function ScheduleList() {
+// 	const schedule = await getSchedule()
+
+// 	return (
+// 		<div className='schedule'>
+// 			{!schedule && <div>Loading...</div>}
+
+// 			{schedule.map((game) => (
+// 				<Link
+// 					href={`/team/schedule/${game.id}`}
+// 					key={game.id}
+// 					className={`card ${game.location}`}>
+// 					<div className='date'>{useDateFormat(game.date).gameDate}</div>
+
+// 					{!game.result && (
+// 						<div className='time'>{useDateFormat(game.date).gameStart}</div>
+// 					)}
+
+// 					<div className='venue'>@ {game.venue}</div>
+
+// 					<div className='matchup'>
+// 						<Image
+// 							src={game.location === 'home' ? homeLogo : game.opponent_logo}
+// 							width={1}
+// 							height={1}
+// 							alt={`${
+// 								game.location === 'home' ? 'Atlanta Braves' : game.opponent
+// 							} Logo`}
+// 							priority
+// 						/>
+// 						<span>vs</span>
+// 						<Image
+// 							src={game.location === 'away' ? homeLogo : game.opponent_logo}
+// 							width={1}
+// 							height={1}
+// 							alt={`${
+// 								game.location === 'away' ? 'Atlanta Braves' : game.opponent
+// 							} Logo`}
+// 							priority
+// 						/>
+// 					</div>
+
+// 					{game.result && (
+// 						<div className={`badge ${game.result}`}>
+// 							<div className='result'>{game.result === 'win' ? 'W' : 'L'}</div>
+// 							<div className='score'>{`${game.win_score}-${game.lose_score}`}</div>
+// 						</div>
+// 					)}
+// 				</Link>
+// 			))}
+// 		</div>
+// 	)
+// }
 export default async function ScheduleList() {
 	const schedule = await getSchedule()
 
@@ -27,47 +106,25 @@ export default async function ScheduleList() {
 			{!schedule && <div>Loading...</div>}
 
 			{schedule.map((game) => (
-				<Link
-					href={`/team/schedule/${game.id}`}
-					key={game.id}
-					className={`card ${game.location}`}>
-					<div className='date'>{useDateFormat(game.date).gameDate}</div>
+				<div
+					key={game.gameID}
+					className={`card`}>
+					<div className='date'>{useDateFormat().gameDateNew(game.gameDate)}</div>
 
-					{!game.result && (
-						<div className='time'>{useDateFormat(game.date).gameStart}</div>
+					{game.gameStatus === 'scheduled' && (
+						<div className='time'>{useDateFormat().gameStartNew(game.gameTime)}</div>
 					)}
-
-					<div className='venue'>@ {game.venue}</div>
 
 					<div className='matchup'>
-						<Image
-							src={game.location === 'home' ? homeLogo : game.opponent_logo}
-							width={1}
-							height={1}
-							alt={`${
-								game.location === 'home' ? 'Atlanta Braves' : game.opponent
-							} Logo`}
-							priority
-						/>
-						<span>vs</span>
-						<Image
-							src={game.location === 'away' ? homeLogo : game.opponent_logo}
-							width={1}
-							height={1}
-							alt={`${
-								game.location === 'away' ? 'Atlanta Braves' : game.opponent
-							} Logo`}
-							priority
-						/>
+						<div className='text-[red]'>{game.away}</div>
+						<span>@</span>
+						<div className='text-[green]'>{game.home}</div>
 					</div>
 
-					{game.result && (
-						<div className={`badge ${game.result}`}>
-							<div className='result'>{game.result === 'win' ? 'W' : 'L'}</div>
-							<div className='score'>{`${game.win_score}-${game.lose_score}`}</div>
-						</div>
+					{game.gameStatus === 'Completed' && (
+						<div className='text-[black]'>{`${game.away} ${game.lineScore.away.R} - ${game.home} ${game.lineScore.home.R}`}</div>
 					)}
-				</Link>
+				</div>
 			))}
 		</div>
 	)
--- app/utils/useDateFormat.js
+++ app/utils/useDateFormat.js
@@ -10,5 +10,18 @@ export const useDateFormat = (dateStr) => {
 		minute: 'numeric',
 	})
 
-	return { gameDate, gameStart }
+	function gameDateNew(dateStr) {
+		const date = new Date(dateStr.slice(0, 4), dateStr.slice(4, 6) - 1, dateStr.slice(6, 8));
+		
+		const options = { year: '2-digit', month: 'numeric', day: 'numeric', weekday: 'short' };
+		return date.toLocaleDateString('en-US', options)
+	}
+
+	function gameStartNew(str) {
+		if (str.length < 2) return null;
+		
+		return str.slice(0, -1) + ' ' + str.slice(-1) + 'm';
+	}
+	
+	return { gameDate, gameStart, gameDateNew, gameStartNew }
 }
