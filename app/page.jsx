dfdfe4281fb6db669065d17c9b6dbe5331ca9bb1
hawkjosh <hawkjosh@icloud.com>
2023-09-19 09:29:22 -0400

    removed global css to handle tooltip on main page and applied it to inline tailwind css

--- app/page.jsx
+++ app/page.jsx
@@ -11,22 +11,22 @@ export default async function HomePage() {
 			<div className='flex flex-col items-center gap-8 mb-6 md:gap-10 xl:gap-12'>
 				<div className="text-2xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">Choose a team...</div>
 				<div
-				className='grid w-2/3 grid-cols-3 gap-4 mx-auto grid-flow-dense place-items-center md:grid-cols-5 md:w-5/6 lg:grid-cols-6'>
+				className='grid w-[70%] grid-cols-3 mx-auto gap-x-1.5 gap-y-9 grid-flow-dense place-items-center md:grid-cols-5 md:w-11/12 lg:grid-cols-6 md:gap-y-10'>
 					{teams.map((team, key) => {
 						const { id: teamId, clubName: teamName } = team
 						return (
 							<Link
 								key={key}
 								href={`/${teamId}`}
-								className='team-link flex items-center justify-center transition-all duration-300 bg-slate-500 rounded-full relative hover:scale-[117.5%] w-20 aspect-square sm:w-24 md:w-28 xl:w-32 hover:mb-4 sm:hover:mb-5 xl:hover:mb-6'>
+								className='relative flex items-center justify-center w-[5rem] transition-transform duration-500 rounded-full group bg-slate-500 aspect-square sm:w-[6rem] md:w-[6.5rem] xl:w-[7rem] hover:scale-125 hover:-translate-y-4'>
 								<Image
 									src={logo.logoUrlCapLt(teamId)}
 									width={100}
 									height={100}
 									alt={`${teamName} Logo`}
-									className='w-12 aspect-square sm:w-16 md:w-20 xl:w-24'
+									className='w-[3rem] transition-transform duration-500 aspect-square sm:w-[3.5rem] md:w-[4rem] xl:w-[4.5rem] group-hover:scale-110'
 								/>
-								<div className='absolute hidden w-full text-xs text-center -translate-x-1/2 -bottom-4 team-tooltip left-1/2 sm:-bottom-5 sm:text-sm xl:-bottom-6 xl:text-base'>
+								<div className='absolute w-full text-xs text-center transition-opacity duration-500 -translate-x-1/2 opacity-0 -bottom-4 left-1/2 sm:-bottom-5 sm:text-sm xl:-bottom-6 xl:text-base group-hover:opacity-100'>
 									{teamName}
 								</div>
 							</Link>
